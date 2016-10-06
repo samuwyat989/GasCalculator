3a4471f377b66c2522432a8b8e00ec25e0fdb9b9
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-10-06 13:33:07 -0400

    Setup

--- GasCalculator/Form1.Designer.cs
+++ GasCalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GasCalculator
 {
-    partial class Form1
+    partial class storeInterface
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tendered = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tendered
+            // 
+            this.tendered.Location = new System.Drawing.Point(34, 30);
+            this.tendered.Name = "tendered";
+            this.tendered.Size = new System.Drawing.Size(100, 20);
+            this.tendered.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(34, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(34, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // storeInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tendered);
+            this.Name = "storeInterface";
+            this.Text = "The Corner Gas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tendered;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- GasCalculator/Form1.cs
+++ GasCalculator/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GasCalculator
 {
-    public partial class Form1 : Form
+    public partial class storeInterface : Form
     {
-        public Form1()
+        public storeInterface()
         {
             InitializeComponent();
         }
